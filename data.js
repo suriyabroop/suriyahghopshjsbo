1b0adafbb0eb95cf501371c49037874eaefee1f9
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-20 19:21:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://tnvillanyt:suriyaskp12@cluster0.tzm8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+    bot_token:'5263369505:AAEo49DpiGIAIiAuNZuGHrtfzkwzXcyaNzI',
+    
+    bot_name:'DGB_RICH_BOT',//bot username without @
+
+    bot_admin:1996486622,// admin telegram id
+
+    channelsList : ['@PokemonAirdrop','@AirdropMajesty','@OfficialEarthQuake','@AirdropComrades','@Legit_BotsOnly','@AirdropJaguar','@Live_AirdropHub','@TNG_TO_MOON'],
+
+    reffer_bonus:0.1,// refferal bonus amount
+
+    min_wd:0.4,//min withdrawal amount
+
+    daily_bonus:0.001,// daily bonus amount
+
+    currency:'DGB', //bot cuurency
+
+    cb_api_key:'EamGXayT3yLF0tUK',//coinbase api key
+
+    cb_api_secret:'QIyYjkevBj4L0WIxB3Ha252kUkYbUG6l',// coinbas>
+
+    cb_account_id:'cc0c63f7-5a5c-5d90-b04f-90083ee239a3',// cou>
+
+    payment_channel:'@TNG_TO_MOON'// payment channel username
+
+    }
